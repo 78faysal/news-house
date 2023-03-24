89a9e33f861483a1235255aab35932e1044ee5ba
mdfaysal <faysal7830@gmail.com>
2023-03-25 03:09:57 +0600

    spinner added

--- script.js
+++ script.js
@@ -20,20 +20,25 @@ const handleCategory = (categories) => {
 
     // Add event listener to links in the navbar
     const navbarLinks = document.querySelectorAll('.nav-link');
+    const spinner = document.getElementById('spinner');
     navbarLinks.forEach(link => {
         link.addEventListener('click', event => {
+            spinner.style.display = 'block';
             event.preventDefault();
             const categoryId = event.target.getAttribute('data-category-id');
             console.log(categoryId)
             fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
                 .then(response => response.json())
-                .then(data => handleNewses(data.data))
+                .then(data => {
+                    handleNewses(data.data); spinner.style.display = 'none';
+                })
                 .catch(err => console.error(err));
         });
     });
 
 
     const handleNewses = (newses) => {
+
         const newsItems = newses.map(news => {
             const thumbnail = news.thumbnail_url;
             const title = news.title;
@@ -55,7 +60,7 @@ const handleCategory = (categories) => {
                                 <p class="card-text">${details.slice(0, 400)}...</p>
                                 
                                 <div class="d-flex justify-content-between align-items-center">
-                                    <div class="d-flex align-items-center g-2">
+                                    <div class="d-flex align-items-center">
                                         <img style=" max-height: 40px" class="rounded-circle img-fluid" src="${img}" alt="">
                                         <div>
                                             <p class="mb-0">${name}</p>
@@ -76,6 +81,19 @@ const handleCategory = (categories) => {
         }).join('');
         const newsItem = document.getElementById("newsItem");
         newsItem.innerHTML = newsItems;
+
+        const errorMessage = document.getElementById("errorMessage");
+        if (newses.length === 0) {
+            errorMessage.innerHTML = `<p class="mb-0">No items found</p>`
+        } else {
+            errorMessage.innerHTML = `<p class="mb-0">${newses.length} items found</p>`
+        }
+
+        // setup for spinner 
+        document.getElementById("spinner").style.display = "block";
+        setTimeout(function () {
+            document.getElementById("spinner").style.display = "none";
+        }, 3000);
     };
 };
 
