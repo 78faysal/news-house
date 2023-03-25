fd4111d1ba60993d37dd6e059b20e1aac3bfadf8
mdfaysal <faysal7830@gmail.com>
2023-03-26 00:25:43 +0600

    navbar config done

--- script.js
+++ script.js
@@ -70,12 +70,34 @@ const handleCategory = (categories) => {
                                     <div>
                                         <p class="mb-0">Views: ${views}</p>
                                     </div>
-                                    <button class="btn btn-secondary">-></button>
+                                    <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">-></button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+
+
+                
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+      <img src="${thumbnail}" class="" alt="...">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <h1 class="modal-title fs-5" id="exampleModalLabel">${title}</h1>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
             
             `
         }).join('');
